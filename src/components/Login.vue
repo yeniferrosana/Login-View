2ae6bac942a56f9cb0ca15191634a7eef1fb5f0f
Yenifer Ramírez <yeniferramirez11@gmail.com>
2022-03-23 21:24:48 -0400

    Estructura de SingIn

---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card-text class="mt-12">
+    <h1
+      class="text-center display-2 blue--text text--darken-4"
+    >Inicia Sesión en Diprella</h1>
+    <div class="text-center mt-4">
+      <v-btn class="mx-2" fab color="black" outlined>
+        <v-icon>fab fa-facebook-f</v-icon>
+      </v-btn>
+
+      <v-btn class="mx-2" fab color="black" outlined>
+        <v-icon>fab fa-google-plus-g</v-icon>
+      </v-btn>
+      <v-btn class="mx-2" fab color="black" outlined>
+        <v-icon>fab fa-linkedin-in</v-icon>
+      </v-btn>
+    </div>
+    <h4 class="text-center mt-4">Asegúrese de su correo electrónico para el registro</h4>
+    <v-form>
+      <v-text-field
+        label="Correo Electrónico"
+        name="Email"
+        prepend-icon="email"
+        type="text"
+        color="blue darken-4"
+      />
+
+      <v-text-field
+        id="password"
+        label="Contraseña"
+        name="password"
+        prepend-icon="lock"
+        type="password"
+        color="blue darken-4"
+      />
+    </v-form>
+    <h3 class="text-center mt-4">¿Olvidaste tu contraseña?</h3>
+  </v-card-text>
+</template>
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="fill-height" fluid>
+    <!--Only for PC-->
+    <v-row align="center" justify="center" class="d-none d-sm-flex">
+      <v-col cols="12" sm="8" md="8">
+        <v-card class="elevation-12">
+          <v-window v-model="step">
+            <v-window-item :value="1">
+              <v-row>
+                <v-col cols="12" md="8">
+                  <Login />
+                  <div class="text-center mt-3 mb-12">
+                    <v-btn rounded color="blue darken-4" dark>INICIAR SESIÓN</v-btn>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="4" class="blue darken-4">
+                  <v-card-text class="white--text mt-12">
+                    <h1 class="text-center display-1">¡Hola, Amigo!</h1>
+                    <h5
+                      class="text-center"
+                    >Ingrese sus datos personales y comience el viaje con nosotros</h5>
+                  </v-card-text>
+                  <div class="text-center">
+                    <v-btn rounded outlined dark @click="step++">REGISTRARSE</v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from "../components/Login";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Login
+  },
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
   }
 }
 </script>
